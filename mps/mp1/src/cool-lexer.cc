45bd635f05df26e7bac4dff25403f4188f7d7bff
Charlie Martell <charliecmartell@gmail.com>
2014-09-15 18:21:17 -0500

    finished?

--- mps/mp1/src/cool-lexer.cc
+++ mps/mp1/src/cool-lexer.cc
@@ -273,8 +273,7 @@ int skipLineComment()
     switch(lookNext())
     {
       case EOF:
-          cool_yylval.error_msg = "EOF in comment";
-          return ERROR;
+          return 0;
       case '\n':
         curr_lineno++;
         getNext();
